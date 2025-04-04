8566dcf54516937273685a49b9e36382da3d29e8
xolboyev <javohirxolboyev0232@gmail.com>
2025-04-05 02:58:12 +0500

    Heroni tugatdim

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,29 +1,44 @@
 import React from "react";
+import Image1 from "../../assets/hero/Image1.png";
+import Image2 from "../../assets/hero/Image2.png";
+import Image3 from "../../assets/hero/Image3.png";
+import Slider from "react-slick"
 
 const ImageList = [
   {
     id: 1,
-    img: "",
+    img: Image2,
     title: "Upto 58% off on all Men's Wear",
     description:
       "Lorem His Life Will forever be Changed dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqiua.",
   },
   {
     id: 2,
-    img: "",
+    img: Image1,
     title: "30% off on all Women's Wear",
     description:
       "Who's there Lorem dolor sit amet , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqiua.",
   },
   {
     id: 3,
-    img: "",
+    img: Image3,
     title: "70% off on all Products Sale",
     description:
       "consectetur adipiscing elit , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   },
 ];
 const Hero = () => {
+  var settings = {
+    dots:false,
+    arrows: false,
+    speed: 800,
+    slidesToScroll:1,
+    autoplay:true,
+    autoplaySpeed:4000,
+    cssEase:"ease-in-out",
+    pouseOnHover: false,
+    pouseOnFocus:true,
+  }
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
       {/* background pattern */}
@@ -33,14 +48,53 @@ const Hero = () => {
       ></div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0">
-        <div>
-          <div className=" grid grid-cols-1  sm:grid-cols-2">
-            {/* text content section  */}
-            <div>
-              
-            </div>
-          </div>
-        </div>
+        <Slider {...settings}>
+
+          {ImageList.map((item)=>(
+
+<div>
+<div className=" grid grid-cols-1  sm:grid-cols-2">
+  {/* text content section  */}
+  <div
+    className="flex flex-col justify-center gap4 pt-12 sm:pt-0 text-center sm:text-left
+  order-2 sm:order-1 relative z-10
+  "
+  >
+    <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+     {item.title}
+    </h1>
+    <p className="text-sm">
+     {item.description}
+    </p>
+    <div>
+      <button
+        className="bg-gradient-to-r from-orange-300 to-orange-400 hover:scale-105
+      duration-200 text-white py-2 px-4 rounded-full
+      "
+      >
+        Order Now{" "}
+      </button>
+    </div>
+  </div>
+  {/* image section */}
+  <div className="order-1 sm:order-2">
+    <div className="relative z-10">
+      <img
+        src={item.img}
+        alt=""
+        className="w-[300px] h-[300px] sm:h-[450px] 
+      sm:w-[450px] sm:scale-105  lg:scale-120 object-contain
+      mx-auto
+      "
+      />
+    </div>
+  </div>
+</div>
+</div>
+          ))}
+      
+        </Slider>
+       
       </div>
     </div>
   );
